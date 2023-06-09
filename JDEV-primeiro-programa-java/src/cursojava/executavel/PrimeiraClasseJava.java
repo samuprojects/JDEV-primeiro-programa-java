d230f9f8fed0380cd89a5f2f60aff628d5a6cdd1
samuel <samuel@jdev.com.br>
2023-06-09 20:19:02 -0300

    Aulas05_05 e 05_06

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,10 +1,11 @@
-package java.executavel;
+/*Aulas com correção do nome package*/
+package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console*/
+		
 		System.out.println("Meu primeiro programa em Java");
 		
 	}
