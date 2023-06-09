6afe3c2393dc8b77541c6285402abced5c5ada66
samuel <samuel@jdev.com.br>
2023-06-09 17:53:37 -0300

    Aula05_04 Primeiro Projeto

---
+++ JDEV-primeiro-programa-java/src/java/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package java.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+		
+	}
+
+}
