ad1654abab21bf58531825bef276be832c0c8249
samuel <samuel@jdev.com.br>
2023-06-10 19:10:02 -0300

    Aula08_05 Operadores Relacionais

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,34 +2,15 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
-		int nota1 = 90;
-		int nota2 = 90;
-		int nota3 = 60;
-		int nota4 = 90;
-		int media = 0;
-
-		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		int nota1 = 100;
+		int nota2 = 100;
 		
-		/*SWITCH e CASE é melhor utilizado em operações exatas bem específicas*/
-		
-		int dia = 3;
-		
-		switch (dia) {
-		case 1:
-			System.out.println("Domingo");
-			break;
-		case 2:
-			System.out.println("Segunda-feira");
-			break;
-		case 3:
-			System.out.println("Terça-feira");
-			break;
-			
-		default: System.out.println("Outro dia qualquer");
-			break;
+		if (nota1 > nota2) {
+			System.out.println("A nota 1 é maior que a nota 2");			
+		} else {
+			System.out.println("As notas são iguais");
 		}
 	}
 }
