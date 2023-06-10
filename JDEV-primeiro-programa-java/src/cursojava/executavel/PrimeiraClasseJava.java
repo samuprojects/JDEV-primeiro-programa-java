03d04d8d041d6320c474067dd678387fbae8ebb9
samuel <samuel@jdev.com.br>
2023-06-10 13:59:08 -0300

    Aula08_01 If_Else

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,19 +1,25 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	
+
 	public static void main(String[] args) {
-	
-	/*Concatenação = Unir ou juntar dados */
-	String nome = "Alex Fernando";
-	String cpf = "488.789.789-85";
-	String telefone = "45 9979-5800";
-	String endereco = "Maringá - PR";
-	int idade = 32;
-	
-	String saida = "Meu nome é: " + nome + " e o CPF: " + cpf + " meu contato é: " + telefone + " e eu moro em: " + endereco + " e tenho de idade: " + idade;
-	
-	System.out.println(saida);	
-	
+
+		int nota1 = 10;
+		int nota2 = 60;
+		int nota3 = 10;
+		int nota4 = 10;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("Aluno aprovado: " + media);
+
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Aluno em recuperação: " + media);
+		} else {
+			System.out.println("Aluno reprovado: " + media);
+		}
+
 	}
 }
