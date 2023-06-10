a425cbb8a22cd614a725f57816658b97a546fb24
samuel <samuel@jdev.com.br>
2023-06-10 12:40:50 -0300

    Aula07_07 Tipo de dados double

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,12 +4,15 @@ public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
 	
-		int nota1 = 90;
-		int nota2 = 70;
-		int nota3 = 50;
-		int nota4 = 88;
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
 		
-		System.out.println("Média das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		/*Com o double passamos a considerar corretamente o resultado quebrado após a vírgula*/
+		System.out.println("Média das notas = " + mediaFinal ); /* No exercício anterior o resultado era arredondado para menos*/
 
 	
 	}
