5cd954a74ef49cb89c56d41fb823b718baaaa799
samuel <samuel@jdev.com.br>
2023-06-10 20:08:35 -0300

    Aula08_06 While e Do While

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,13 +4,22 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		int nota1 = 100;
-		int nota2 = 100;
+		/* Estrutura de repetição while (conceito de repetição enquanto for verdadeiro porém necessário a mudança para encerrar o código quando for falso */
 		
-		if (nota1 > nota2) {
-			System.out.println("A nota 1 é maior que a nota 2");			
-		} else {
-			System.out.println("As notas são iguais");
+		int numero = 0;
+		
+		while (numero <= 60) {/* 1º verificação, depois execução */
+			
+			System.out.println("O número atual é: " + numero);
+			numero++;
 		}
+		
+		System.out.println("----------------------------------");
+		
+		int numero2 = 0;
+		do { /* 1º execução, depois verificação */
+			System.out.println("O número atual é: " + numero2);
+			numero2++;
+		} while (numero2 <= 60);
 	}
 }
