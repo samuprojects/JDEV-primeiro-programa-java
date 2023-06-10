ec4126db7677c3f0cc374330bd85d7eb19876871
samuel <samuel@jdev.com.br>
2023-06-10 12:28:28 -0300

    Aula07_05 - Variáveis local e global

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,23 +2,24 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	@SuppressWarnings("unused")
+	/* variável global acessível a todos e com valor compartilhado*/
+	static int maiorIdadeGlobal = 30;
+	
 	public static void main(String[] args) {
-		
+	
+	/*variável local pertence somente a este método assim como o seu valor*/	
 	int maiorIdade = 18;
 	
-	int idosoIdade = 60;
-	
-	int jovemIdade = 16;
-	
-	int criancaIdade = 7;
-	
-	String cpf = "145.785.789-54";
-	
-	System.out.println("Maior idade é = " + maiorIdade);
-	System.out.println("Idoso idade é = " + idosoIdade);
-	
+	System.out.println("Valor da variável local = " + maiorIdade);
+	System.out.println("Valor da variável Global = " + maiorIdadeGlobal);
+	metodo2();
 		
+	}
+	
+	@SuppressWarnings("unused")
+	public static void metodo2() {
+		int mediaAno = 50; /* variável local metodo2() */
+		System.out.println("Valor da variável Global = " + maiorIdadeGlobal);
 	}
 
 }
