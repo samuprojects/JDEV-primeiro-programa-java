5030726fc8796d32a8173b60cda1ecb7294b4c29
samuel <samuel@jdev.com.br>
2023-06-10 14:29:18 -0300

    Aula08_03 Operações lógicas aninhadas

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,27 +4,29 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		int nota1 = 10;
-		int nota2 = 60;
-		int nota3 = 10;
-		int nota4 = 10;
+		int nota1 = 90;
+		int nota2 = 90;
+		int nota3 = 60;
+		int nota4 = 90;
 		int media = 0;
 
 		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		/*Operações lógicas aninhadas são operações dentro de operações*/
 
-		if (media >= 70) {
-			System.out.println("Aluno aprovado: " + media);
-
-		} else if (media >= 40 && media <= 69) {
-			System.out.println("Aluno em recuperação: " + media);
+		if (media >= 50) {
+			if (media >= 70) {
+				if (media > 90) {
+					System.out.println("Aluno está aprovado direto - Parabens " + media);
+				} else {
+					System.out.println("Aluno está aprovado direto " + media);
+				}
+			} else {
+				System.out.println("Aluno está em recuperação: " + media);
+			}
 		} else {
-			System.out.println("Aluno reprovado: " + media);
+			System.out.println("Aluno reprovado direto: " + media);
 		}
-		
-		/*Operadores ternários são recomendados em micro validações*/
-		String saidaResultado = media >= 70 ? "Aluno Aprovado" : (media >= 40 && media <= 69) ? "Aluno em recuperação" : "Reprovado";
-		
-		System.out.println(saidaResultado);
 
 	}
 }
