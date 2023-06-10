67730be778b938f16dd9f0224fca13a5054c5fad
samuel <samuel@jdev.com.br>
2023-06-10 13:08:44 -0300

    Aula07_10 Concatenação de variáveis

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,14 +2,18 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 	
-	/*String no Java é utilizado para textos de tamanho qualquer */
-	String nome;
-	String cpf;
-	String telefone;
-	String endereco;
+	/*Concatenação = Unir ou juntar dados */
+	String nome = "Alex Fernando";
+	String cpf = "488.789.789-85";
+	String telefone = "45 9979-5800";
+	String endereco = "Maringá - PR";
+	int idade = 32;
+	
+	String saida = "Meu nome é: " + nome + " e o CPF: " + cpf + " meu contato é: " + telefone + " e eu moro em: " + endereco + " e tenho de idade: " + idade;
+	
+	System.out.println(saida);	
 	
 	}
 }
