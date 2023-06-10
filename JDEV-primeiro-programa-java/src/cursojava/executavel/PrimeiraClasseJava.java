7aea1529a9cbcd4045d0edbb14e51457b501101a
samuel <samuel@jdev.com.br>
2023-06-10 20:46:17 -0300

    Aula08_08 Utilização do FOR com Break

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,16 +4,26 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição FOR */
-		
+		/* Estrutura de repetição FOR com Break (Parada) */
+
 		for (int numero = 0; numero <= 10; numero++) {
-			System.out.println("Número atual é: " + numero);
+			System.out.println(numero);
+			if (numero == 3) {
+				System.out.println("Obaaa, encontrei o numero " + numero);
+				System.out.println("Estou parando de executar...");
+				break;
+			}			
 		}
-		
+
 		System.out.println("-----------------------------");
-		
+
 		for (int numero = 10; numero >= 0; numero--) {
-			System.out.println("Número atual é: " + numero);
+			System.out.println(numero);
+			if (numero == 3) {
+				System.out.println("Obaaa, encontrei o numero " + numero);
+				System.out.println("Estou parando de executar...");
+				break;
+			}	
 		}
 	}
 }
