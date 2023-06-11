541a2d9787b6dd23a46819ee591269ca235aaf30
samuel <samuel@jdev.com.br>
2023-06-10 21:19:02 -0300

    Aula08_10 Resto da divisão com MOD

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,28 +4,13 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/* Estrutura de repetição FOR e Continue */
+		/* Módulo: Resto da divisão % */
 
-		System.out.println("Contando de 0 a 10...");
-		for (int numero = 0; numero <= 10; numero++) {
-			if (numero == 3 || numero == 6 || numero == 9) {
-				System.out.println("Obaaa, encontrei o número " + numero);
-				continue;
-			}
-
-			System.out.println("Processando o laço de repetição...");
-		}
-
-		System.out.println("-----------------------------");
-
-		System.out.println("Contando de 10 a 0...");
-		for (int numero = 10; numero >= 0; numero--) {
-			if (numero == 3 || numero == 6 || numero == 9) {
-				System.out.println("Obaaa, encontrei o número " + numero);
-				continue;
-			}
-
-			System.out.println("Processando o laço de repetição");
-		}
+		double carro = 9;
+		double pessoa = 2;
+		
+		double resto = carro % pessoa; /* MOD */
+		
+		System.out.println("Sobraram exatamente: " + resto + " carro(s).");
 	}
 }
