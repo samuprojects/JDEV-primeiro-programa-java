d8870f8b68de31e37189bd57a81181205bc5e70a
samuel <samuel@jdev.com.br>
2023-06-12 15:09:23 -0300

    Aula09_14 Automatizando dados Entrada com JOptionPane

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,54 +1,53 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		String idade = JOptionPane.showInputDialog("Qual a idade?");
+		String dataNascimento = JOptionPane.showInputDialog("Data de nascimento?");
+		String rg = JOptionPane.showInputDialog("Registro Geral?");
+		String cpf = JOptionPane.showInputDialog("Qual é o CPF?");
+		String mae = JOptionPane.showInputDialog("Nome da mãe?");
+		String pai = JOptionPane.showInputDialog("Nome do pai?");
+		String matricula = JOptionPane.showInputDialog("Data da matrícula?");
+		String serie = JOptionPane.showInputDialog("Qual a série?");
+		String escola = JOptionPane.showInputDialog("Nome da escola?");
+		String note1 = JOptionPane.showInputDialog("Nota 1?");
+		String note2 = JOptionPane.showInputDialog("Nota 2?");
+		String note3 = JOptionPane.showInputDialog("Nota 3?");
+		String note4 = JOptionPane.showInputDialog("Nota 4?");
 
 		Aluno aluno1 = new Aluno();
 
-		aluno1.setNome("João da Silva");
-		aluno1.setIdade(50);
-		aluno1.setDataNascimento("18/10/1987");
-		aluno1.setRegistroGeral("44554.4544.545");
-		aluno1.setNumeroCpf("54544.545454.5454");
-		//aluno1 = null; // essa linha descomentada causará um NullPointerException na linha 18 porque o objeto se tornou nulo.
-		aluno1.setNomeMae("Shirlei"); // para solucionar basta corrigir o que está nulo
-		aluno1.setNomePai("Antonio");
-		aluno1.setDataMatricula("10/01/2019");
-		aluno1.setSerieMatriculado("5");
-		aluno1.setNomeEscola("Escola JDEV Treinamento");
-		aluno1.setNota1(90);
-		aluno1.setNota2(80.8);
-		aluno1.setNota3(70.9);
-		aluno1.setNota4(90.7);
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setRegistroGeral(rg);
+		aluno1.setNumeroCpf(cpf);
+		aluno1.setNomeMae(mae);
+		aluno1.setNomePai(pai);
+		aluno1.setDataMatricula(matricula);
+		aluno1.setSerieMatriculado(serie);
+		aluno1.setNomeEscola(escola);
+		aluno1.setNota1(Double.parseDouble(note1));
+		aluno1.setNota2(Double.parseDouble(note2));
+		aluno1.setNota3(Double.parseDouble(note3));
+		aluno1.setNota4(Double.parseDouble(note4));
 
 		System.out.println("Nome é = " + aluno1.getNome());
 		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("Nome mãe = " + aluno1.getNomeMae());
 		System.out.println("Nascimento = " + aluno1.getDataNascimento());
 		System.out.println("Média da nota é = " + aluno1.getMediaNota());
 		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
 		System.out.println("Resultado 2 = " + aluno1.getAlunoAprovado2());
-
-		System.out.println("--------------------------------");
-
-		Aluno aluno2 = new Aluno();
-
-		aluno2.setNome("Pedro");
-		aluno2.setIdade(40);
-		aluno2.setDataNascimento("10/10/1970");
-
-		System.out.println("Aluno 2 Nome é: " + aluno2.getNome());
-		System.out.println("Aluno 2 Idade é: " + aluno2.getIdade());
-		System.out.println("Aluno 2 Nascimento = " + aluno2.getDataNascimento());
-
-		Aluno aluno3 = new Aluno();
-
-		Aluno aluno4 = new Aluno("Maria");
-
-		Aluno aluno5 = new Aluno("José", 50);
 
 	}
 }
