3cabf7342e8fada3cc5066a60619d9aefc9f4184
samuel <samuel@jdev.com.br>
2023-06-12 12:05:36 -0300

    Aula09_11 Método para cálculo de média de nota

--- JDEV-primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ JDEV-primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -13,6 +13,11 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+
 	public Aluno() {
 
 	}
@@ -26,22 +31,11 @@ public class Aluno {
 		idade = idadePadrao;
 	}
 
-	/* Métodos Getters e Setters para alterar os atributos dos objetos */
-	/* GET (resgatar ou obter) */
-	/* SET (adicionar ou receber) */
-
-	/*
-	 * o operador this abaixo vai indicar o atributo da classe (objeto) como
-	 * parametro, liberando o mesmo nome para os parametros locais do método
-	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	public String getNome() { /*
-								 * atenção para a convenção (Camel Case) na declaração dos métodos get e set com
-								 * os atributos
-								 */
+	public String getNome() {
 		return this.nome;
 	}
 
@@ -116,4 +110,42 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() { /* get usa return para devolver o valor */
+		return nota4;
+	}
+
+	public void setNota4(double nota4) { /* set usa void pois não devolve nenhum valor */
+		this.nota4 = nota4;
+	}
+
+	/* Método que retorna a média do aluno */
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+
 }
--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -6,9 +6,9 @@ public class PrimeiraClasseJava {
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		
+
 		Aluno aluno1 = new Aluno();
-		
+
 		aluno1.setNome("João da Silva");
 		aluno1.setIdade(50);
 		aluno1.setDataNascimento("18/10/1987");
@@ -19,27 +19,32 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula("10/01/2019");
 		aluno1.setSerieMatriculado("5");
 		aluno1.setNomeEscola("Escola JDEV Treinamento");
-		
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90.7);
+
 		System.out.println("Nome é = " + aluno1.getNome());
 		System.out.println("Idade é = " + aluno1.getIdade());
 		System.out.println("Nascimento = " + aluno1.getDataNascimento());
+		System.out.println("Média da nota é = " + aluno1.getMediaNota());
 
 		System.out.println("--------------------------------");
-				
+
 		Aluno aluno2 = new Aluno();
-		
+
 		aluno2.setNome("Pedro");
 		aluno2.setIdade(40);
 		aluno2.setDataNascimento("10/10/1970");
-		
+
 		System.out.println("Aluno 2 Nome é: " + aluno2.getNome());
 		System.out.println("Aluno 2 Idade é: " + aluno2.getIdade());
 		System.out.println("Aluno 2 Nascimento = " + aluno2.getDataNascimento());
-		
+
 		Aluno aluno3 = new Aluno();
-		
+
 		Aluno aluno4 = new Aluno("Maria");
-		
+
 		Aluno aluno5 = new Aluno("José", 50);
 
 	}
