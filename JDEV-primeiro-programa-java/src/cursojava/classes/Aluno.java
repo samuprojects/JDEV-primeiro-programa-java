66b18da1ef3ad6fee519cbf22f1935f51558dc6d
samuel <samuel@jdev.com.br>
2023-06-12 11:49:09 -0300

    Aula09_10 Métodos Getters e Setters

--- JDEV-primeiro-programa-java/src/cursojava/classes/Aluno.java
+++ JDEV-primeiro-programa-java/src/cursojava/classes/Aluno.java
@@ -1,31 +1,119 @@
 package cursojava.classes;
 
-public class Aluno { /* A classe com visibilidade public pôde ser criada na Primeira ClasseJava */
+public class Aluno {
 
-	/* para acessar podemos alterar para public apenas de forma didática como a seguir */
-	public String nome;
-	public int idade;
-	String dataNascimento; /* porém os demais atributos não podem ser acessados por que estão private */
-	@SuppressWarnings("unused")
-	private String registroGeral; /* a variável acima por padrão já é private e podem ser declarados explicitamente como aqui */
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMatriculado;
-	
-	public Aluno() { /* Criar o objeto na memória - Construtor Padrão do Java */
-				
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+
+	public Aluno() {
+
 	}
-	
-	public Aluno (String nomePadrao) {/* Outro construtor - sobrecarga de construtor */
+
+	public Aluno(String nomePadrao) {
 		nome = nomePadrao;
 	}
-	
-	public Aluno (String nomePadrao, int idadePadrao) {
+
+	public Aluno(String nomePadrao, int idadePadrao) {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
 
+	/* Métodos Getters e Setters para alterar os atributos dos objetos */
+	/* GET (resgatar ou obter) */
+	/* SET (adicionar ou receber) */
+
+	/*
+	 * o operador this abaixo vai indicar o atributo da classe (objeto) como
+	 * parametro, liberando o mesmo nome para os parametros locais do método
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() { /*
+								 * atenção para a convenção (Camel Case) na declaração dos métodos get e set com
+								 * os atributos
+								 */
+		return this.nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
 }
--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -8,13 +8,33 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno();
-		aluno1.nome = "João"; /* não é indicado deixar public, a solução será apresentada na aula seguinte */
-		aluno1.idade = 50;
 		
-		System.out.println("Nome no aluno1 é: " + aluno1.nome);
-		System.out.println(("Idade é = " + aluno1.idade));
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("44554.4544.545");
+		aluno1.setNumeroCpf("54544.545454.5454");
+		aluno1.setNomeMae("Shirlei");
+		aluno1.setNomePai("Antonio");
+		aluno1.setDataMatricula("10/01/2019");
+		aluno1.setSerieMatriculado("5");
+		aluno1.setNomeEscola("Escola JDEV Treinamento");
 		
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("Nascimento = " + aluno1.getDataNascimento());
+
+		System.out.println("--------------------------------");
+				
 		Aluno aluno2 = new Aluno();
+		
+		aluno2.setNome("Pedro");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("10/10/1970");
+		
+		System.out.println("Aluno 2 Nome é: " + aluno2.getNome());
+		System.out.println("Aluno 2 Idade é: " + aluno2.getIdade());
+		System.out.println("Aluno 2 Nascimento = " + aluno2.getDataNascimento());
 		
 		Aluno aluno3 = new Aluno();
 		
