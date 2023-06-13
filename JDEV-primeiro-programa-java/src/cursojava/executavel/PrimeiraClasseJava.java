475b430bb83fab319e0146a292a0928fd1747ff7
samuel <samuel@jdev.com.br>
2023-06-13 11:06:47 -0300

    Aula09_18 Relacionando Aluno com novo objeto Disciplina

--- JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -46,15 +46,15 @@ public class PrimeiraClasseJava {
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);
 		
-		aluno1.setNota1(Double.parseDouble(note1));
-		aluno1.setNota2(Double.parseDouble(note2));
-		aluno1.setNota3(Double.parseDouble(note3));
-		aluno1.setNota4(Double.parseDouble(note4));
+		aluno1.getDisciplina().setNota1(Double.parseDouble(note1)); /* Alterado para incluir os relacionamentos */
+		aluno1.getDisciplina().setNota2(Double.parseDouble(note2));
+		aluno1.getDisciplina().setNota3(Double.parseDouble(note3));
+		aluno1.getDisciplina().setNota4(Double.parseDouble(note4));
 		
-		aluno1.setDisciplina1(disciplina1);
-		aluno1.setDisciplina1(disciplina2);
-		aluno1.setDisciplina1(disciplina3);
-		aluno1.setDisciplina1(disciplina4);
+		aluno1.getDisciplina().setDisciplina1(disciplina1);
+		aluno1.getDisciplina().setDisciplina2(disciplina2);
+		aluno1.getDisciplina().setDisciplina3(disciplina3);
+		aluno1.getDisciplina().setDisciplina4(disciplina4);
 
 		System.out.println(aluno1);
 		System.out.println("Média do aluno = " + aluno1.getMediaNota());
