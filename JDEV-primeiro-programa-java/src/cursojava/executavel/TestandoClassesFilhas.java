2cf7c2db89552cb35925e03d6f0fd9f97cae9567
samuel <samugit.projects@gmail.com>
2023-06-16 10:25:38 -0300

    Aula10_08 - Entendendo Polimorfismo

--- JDEV-primeiro-programa-java/src/cursojava/executavel/TestandoClassesFilhas.java
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/TestandoClassesFilhas.java
@@ -2,6 +2,7 @@ package cursojava.executavel;
 
 import cursojava.classes.Aluno;
 import cursojava.classes.Diretor;
+import cursojava.classes.Pessoa;
 import cursojava.classes.Secretario;
 
 public class TestandoClassesFilhas {
@@ -22,6 +23,7 @@ public class TestandoClassesFilhas {
 		secretario.setExperiencia("Administração");
 		secretario.setNumeroCpf("0440455040");
 		secretario.setIdade(18);
+		secretario.setNome("João");
 		
 		System.out.println(aluno);
 		System.out.println(diretor);
@@ -30,11 +32,19 @@ public class TestandoClassesFilhas {
 		System.out.println(aluno.pessoaMaiorIdade() + " - " + aluno.msgMaiorIdade());
 		System.out.println(diretor.pessoaMaiorIdade());
 		System.out.println(secretario.pessoaMaiorIdade());
-		
+				
 		System.out.println("Salário Aluno é = " + aluno.salario());
 		System.out.println("Salário Diretor é = " + diretor.salario());
 		System.out.println("Salário Secretário é = " + secretario.salario());
+		
+		teste(aluno);
+		teste(diretor);
+		teste(secretario);
 
 	}
-
+	
+	public static void teste(Pessoa pessoa) {
+		System.out.println("Essa pessoa é demais = " + pessoa.getNome() + " e o salário é de = " + pessoa.salario());
+	}
+	
 }
