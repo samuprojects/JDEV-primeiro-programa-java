eeef929ae4a42c918334fcbe9fd2b79b1d439697
samuel <samugit.projects@gmail.com>
2023-06-21 11:45:16 -0300

    Aulas13_01 a 13_02 - Arrays: Definição e criação

---
+++ JDEV-primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -0,0 +1,21 @@
+package cursojava.executavel;
+
+public class ArrayVetor {
+	
+	public static void main(String[] args) {
+		
+		// Array pode ser de todos os tipos de dados
+		// e deve ter a quantidade de posições definidas
+		
+		double[] notas = new double[4];
+		
+		// Atribuição de valores individualmente nas posições
+		notas[0] = 7.8;
+		notas[1] = 8.7;
+		notas[2] = 9.7;
+		notas[3] = 9.9;
+		
+		System.out.println(notas);
+	}
+
+}
