61476f573effd3f2b8bbf8a8de5922fce0350d5b
samuel <samugit.projects@gmail.com>
2023-06-23 18:24:35 -0300

    Aula14_09 Controlando a Thead 1 com Start e Stop

--- JDEV-primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ JDEV-primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -26,7 +30,24 @@ public class TelaTimeThread extends JDialog {
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
 
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			while (true) { // loop infinito até que seja pressionado stop
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
 	
+	private Thread thread1Time;
 	
 	@SuppressWarnings("static-access")
 	public TelaTimeThread() { // Executa o que estiver dentro no momento da instanciação do objeto
@@ -71,6 +92,28 @@ public class TelaTimeThread extends JDialog {
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridx++;
 		jPanel.add(jButton2, gridBagConstraints);
+		
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) { // Executa o clique no botão
+				
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+
+				
+			}
+		});
+		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@SuppressWarnings("deprecation")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time.stop();
+				
+			}
+		});
 		
 		add(jPanel, BorderLayout.WEST);
 		// Sempre será o último comando
