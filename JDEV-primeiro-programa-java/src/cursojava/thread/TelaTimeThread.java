f0e88bc946a430e51c1306c2a1e6c6a2bd223314
samuel <samugit.projects@gmail.com>
2023-06-23 12:36:31 -0300

    Aula14_07 Conhecendo o GridBagConstraints gerenc. posicionam. e layout

--- JDEV-primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ JDEV-primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -1,11 +1,26 @@
 package cursojava.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class TelaTimeThread extends JDialog {
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout()); // Painel de componentes
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+	private JTextField mostraTempo2 = new JTextField();
+	
 	
 	public TelaTimeThread() { // Executa o que estiver dentro no momento da instanciação do objeto
 		
@@ -15,8 +30,26 @@ public class TelaTimeThread extends JDialog {
 		setResizable(false);
 		// Primeira parte concluída
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints(); // Controlador de posicionamento de componentes
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
 		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(mostraTempo2, gridBagConstraints);
+		
+		add(jPanel, BorderLayout.WEST);
 		// Sempre será o último comando
 		setVisible(true); // Torna a tela visível para o usuário
 	}
