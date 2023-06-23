1775f919f5bca5a00baec9bf1da758bd413e1a66
samuel <samugit.projects@gmail.com>
2023-06-23 11:51:40 -0300

    Aula14_05 Thread em objeto e o Runnable

--- JDEV-primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ JDEV-primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -7,49 +7,15 @@ public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
 		
 		// Thread processando em paralelo do envio de email
-		new Thread() {
-			
-			public void run() { 
-				
-				
-				for (int pos = 0 ; pos < 10; pos++) {
-					
-					System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
-					
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-
-				}				
-			}
-			
-		}.start();
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 		
 		
 		// ******************* DIVISÃO DAS THREADS ************************
 		
 		// Thread processando em paralelo do envio de nota fiscal
-				new Thread() {
-					
-					public void run() {
-						
-						
-						for (int pos = 0 ; pos < 10; pos++) {
-							
-							System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
-							
-							try {
-								Thread.sleep(2000);
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-
-						}						
-					}
-					
-				}.start();
+		Thread threadNFCE = new Thread(thread2);
+		threadNFCE.start();
 		
 		
 		// Código do sistema do usuário continua o fluxo de trabalho
@@ -59,5 +25,41 @@ public class AulaThread {
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 		
 	}
+	
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			for (int pos = 0 ; pos < 10; pos++) {
+				
+				System.out.println("Executando alguma rotina, por exemplo envio de nota fiscal");
+				
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	};
+	
+	private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			for (int pos = 0 ; pos < 10; pos++) {
+				
+				System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	};
 
 }
