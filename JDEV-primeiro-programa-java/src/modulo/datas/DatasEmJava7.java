c96d163e828f88a705cb8531e7136fc8970d113c
samuel <samugit.projects@gmail.com>
2023-07-04 10:25:36 -0300

    Aula15_11 Tempo de processo com Instant

--- JDEV-primeiro-programa-java/src/modulo/datas/DatasEmJava7.java
+++ JDEV-primeiro-programa-java/src/modulo/datas/DatasEmJava7.java
@@ -1,11 +1,13 @@
 package modulo.datas;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DatasEmJava7 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		LocalDate localDate = LocalDate.now();
 		System.out.println("Data atual: " + localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyy")));
@@ -24,6 +26,28 @@ public class DatasEmJava7 {
 		System.out.println("Ano: " + localDate.getYear());
 
 		
+//*****************************************************************************************		
+		System.out.println("****************************************************************");
+		
+		Instant inicio = Instant.now();
+		
+		
+		Thread.sleep(2000); // Pode ser um processo com tempo qualquer que não conhecemos
+		
+		Instant ifinal = Instant.now();
+		
+		Duration duracao = Duration.between(inicio, ifinal);
+		
+		System.out.println("Duração em nano segundos: " + duracao.toNanos());
+		
+		System.out.println("Duração em minutos: " + duracao.toMinutes());
+
+		System.out.println("Duração em horas: " + duracao.toHours());
+
+		System.out.println("Duração em milisegundos: " + duracao.toMillis());
+
+		System.out.println("Duração em dias: " + duracao.toDays());
+
 	}
 
 }
